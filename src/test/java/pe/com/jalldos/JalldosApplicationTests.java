8080ac43a2edced284d24a3a72c0854480076064
Savaaaaj <jhair_2001@hotmail.com>
2022-11-10 13:17:13 -0500

    Subida inicial
    subida inicial del proyecto

---
+++ src/test/java/pe/com/jalldos/JalldosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.jalldos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JalldosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
